a3e7548cf6c5df89519754ad900b5dac1594a150
wentaoStudy <2335844083@qq.com>
2020-07-11 14:17:43 +0800

    添加跳过验证的Never

--- autorun.py
+++ autorun.py
@@ -1,9 +1,9 @@
 import os
 
 # 向pacman.conf添加内容
-# with open("pacman.conf" ,'r') as pacman_conf:
-#     with open("/etc/pacman.conf" , 'a') as conf:
-#         conf.writelines(pacman_conf.readlines())
+with open("pacman.conf" ,'r') as pacman_conf:
+    with open("/etc/pacman.conf" , 'a') as conf:
+        conf.writelines(pacman_conf.readlines())
 
 with open("/etc/yaourtrc" , 'a') as conf:
     conf.write("AURURL=\"https://aur.tuna.tsinghua.edu.cn\"")
